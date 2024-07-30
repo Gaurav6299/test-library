f75955a1877634278e1d4cc47ef6a6e1315fe6fb
Gaurav03 <Kumar.Gaurav03@motherson.com>
2024-07-30 09:55:30 +0530

    add file

---
+++ projects/shared/src/lib/first/first.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-first',
+  standalone: true,
+  imports: [],
+  templateUrl: './first.component.html',
+  styleUrl: './first.component.css'
+})
+export class FirstComponent {
+
+}
